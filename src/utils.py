07190e69e8ff9a3b590909df550c2cdc502dcb43
EE <williamencabopimentel@gmail.com>
2026-02-12 04:32:01 +0000

    Use path helpers

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,13 @@
 import numpy as np
 from neural_network import NeuralNetwork
 import json
+from paths import here
 
 def save_model(neural_network: NeuralNetwork, filename: str) -> None:
     """
     saves a model after training to models/ as a .npz (concise and efficient file format for saving models)
     """
-    filepath = f"../models/{filename}"
+    filepath = here("models", filename)
     weights_biases = {}
     for i, layer in enumerate(neural_network.layers):
         weights_biases[f"layer_{i}_W"] = layer.W
@@ -18,7 +19,7 @@ def load_model(neural_network: NeuralNetwork, filename: str) -> None:
     """
     loads a saved model as a .npz from models/ using a specified name
     """
-    filepath = f"../models/{filename}"
+    filepath = here("models", filename)
     data = np.load(filepath)
     for i, layer in enumerate(neural_network.layers):
         layer.W = data[f"layer_{i}_W"]
@@ -27,10 +28,10 @@ def load_model(neural_network: NeuralNetwork, filename: str) -> None:
 
 def save_results(results: dict, filename: str = "results.json") -> None:
     """
-    saves a model's results as a .json to models/
+    saves a model's results as a .json to results/
     default filename is results.json
     """
-    filepath = f"../results/{filename}"
+    filepath = here("results", filename)
     with open(filepath, "w") as f:
         json.dump(results, f, indent=2)
     print(f"Results saved to {filepath}")
