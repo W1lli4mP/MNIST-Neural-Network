4b02e4df316df7f2c68ad8a439312f34b37dac3f
EE <williamencabopimentel@gmail.com>
2026-02-13 23:38:54 +0000

    Refactor API and design

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,20 +1,13 @@
 import numpy as np
 from config import LEARNING_RATE
 
-# activation and loss functions/derivatives
+# activation functions
 def ReLU(z: np.ndarray) -> np.ndarray:
     return np.maximum(0, z)
 
 def ReLU_derivative(z: np.ndarray) -> np.ndarray:
     return (z > 0).astype(float)
 
-def loss_function(a: np.ndarray, y: np.ndarray) -> np.ndarray:
-    return np.sum((a - y) ** 2)
-
-def loss_function_derivative(a: np.ndarray, y: np.ndarray) -> np.ndarray:
-    return (a - y) # with respect to a
-
-# new activation and loss functions
 def softmax(z: np.ndarray) ->np.ndarray:
     # softmax depends on relative differences, so reduce the number size by subtracting by the maximum to avoid stuff like (inf/ inf)
     z = z - np.max(z, axis=-1, keepdims=True)
@@ -22,13 +15,43 @@ def softmax(z: np.ndarray) ->np.ndarray:
     exp_sum = np.sum(ez, axis=-1, keepdims=True) # axis = -1 sums up all of the classes to 1 and not anything else
     return ez / exp_sum
 
-def cross_entropy(a: np.ndarray, y: np.ndarray) -> np.ndarray:
+# softmax derivative is not required due to delta being simplified with cross-entropy derivative
+
+# loss functions
+def mean_squared_error(y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
+    return np.sum((y_hat - y) ** 2)
+
+def mean_squared_error_derivative(y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
+    return 2 * (y_hat - y)
+
+def cross_entropy(y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
     epsilon = 1e-10 # to avoid log(0)
-    return -np.sum(y * np.log(a + epsilon))
+    return -np.sum(y * np.log(y_hat + epsilon))
+
+# cross-entropy derivative is not required due to delta being simplified with softmax derivative
+
+# activation and loss configuration
+def hidden_activation_function(z: np.ndarray) -> np.ndarray:
+    return ReLU(z)
+
+def hidden_activation_function_derivative(z: np.ndarray) -> np.ndarray:
+    return ReLU_derivative(z)
+
+def output_activation_function(z: np.ndarray) -> np.ndarray:
+    return softmax(z)
+
+def output_activation_function_derivative(z: np.ndarray) -> np.ndarray:
+    return # don't need it for softmax + cross-entropy
+
+def loss_function(a: np.ndarray, y: np.ndarray) -> np.ndarray:
+    return np.sum((a - y) ** 2)
+
+def loss_function_derivative(a: np.ndarray, y: np.ndarray) -> np.ndarray:
+    return (a - y) # with respect to a
 
 # neural network and layer classes
 class NeuralNetwork:
-    def __init__(self, layer_dims: list[int]):
+    def __init__(self, layer_dims: list[int]) -> None:
         # layer dims is a list compiled of the number of nodes in each layer (e.g. 784, 128, 10 means 784 inputs, 128 hidden, 10 outputs)
         self.layers = []
         for i in range(len(layer_dims) - 1):
@@ -44,24 +67,37 @@ class NeuralNetwork:
             else:
                 self.layers.append(Layer(biases, weights))
     
-    def forward(self, x):
+    def forward(self, x: np.ndarray) -> np.ndarray:
         self.activations = [x]
         for layer in self.layers:
             x = layer.forward(x)
             self.activations.append(x)
         return x
     
-    def backward(self, x, y, batch_size):
-        dLda = loss_function_derivative(self.activations[-1], y) # obtain last activation (from output layer)
+    def backward(self, y: np.ndarray, batch_size: int) -> None:
+        # do backpropagation + gradient descent on output layer first
+        output_layer = self.layers[-1]
 
-        # traverse layers in reverse (its backprop)
-        for i in range(len(self.layers) - 1, -1, -1):
-            layer = self.layers[i]
-            layer_input = self.activations[i] # activation from previous layer
+        ## retrieve the output layer's activation (called y_hat instead of a)
+        y_hat = self.activations[-1]
+
+        # get previous layer's activation
+        a_prev = self.activations[-2]
+
+        # get upstream gradient
+        dLda = output_layer.backward(y_hat, a_prev, y, batch_size)
+
+        # traverse HIDDEN layers in reverse order for backpropagation
+        # NOTE: directions are relative to the forward direction despite traversal order
+        for i in range(len(self.layers) - 2, -1, -1):
+            current_layer = self.layers[i]
+
+            # retrieve activation from the previous layer's activations
+            a_prev = self.activations[i]
+            a = self.activations[i + 1]
             
-            # compute gradients and apply gradient descent
-            z = layer.linearity(layer_input)
-            dLda = layer.backward(layer_input, z, dLda, batch_size) # calculate new upstream gradient and apply it via backpropagation
+            # backpropagate to find upstream gradient (pre-gradient descent) for preceding layer and apply gradient descent
+            dLda = current_layer.backward(a, a_prev, dLda, batch_size)
 
 class Layer:
     def __init__(self, biases: np.ndarray, weights: np.ndarray) -> None:
@@ -69,42 +105,53 @@ class Layer:
         self.W = weights
 
     def activation_function(self, z: np.ndarray) -> np.ndarray:
-        return ReLU(z)
+        return hidden_activation_function(z)
 
     def activation_function_derivative(self, z: np.ndarray) -> np.ndarray:
         return ReLU_derivative(z)
 
-    def linearity(self, x: np.ndarray) -> np.ndarray:
+    def get_preactivation(self, x: np.ndarray) -> np.ndarray:
         # calculate weighted sum x1w1 + x2w2 + ... + xnwn + b
         z = x @ self.W + self.b # @ means multiply for matrix products
         return z
 
     def forward(self, x: np.ndarray) -> np.ndarray:
-        z = self.linearity(x)
+        z = self.get_preactivation(x)
         a = self.activation_function(z)
         return a
     
-    def backward(self, x: np.ndarray, z: np.ndarray, dLda: np.ndarray, batch_size: int) -> np.ndarray:
-        """
-        dLda is the upstream gradient
-        returns dLda_prev which is the upstream gradient of the previous layer
-        """
-        dadz = self.activation_function_derivative(z)
-        # dLda = loss_function_derivative(a, y) # (upstream gradient)
+    def backward(self, a: np.ndarray, x: np.ndarray, dLda: np.ndarray, batch_size: int) -> np.ndarray:
+        ## compute delta
+        # dLdz = dLda * dadz
+        z = self.get_preactivation(x)
+        delta = dLda * self.activation_function_derivative(z)
 
-        dLdz = dadz * dLda # delta (upstream gradient at pre activation)
+        ## calculate upstream gradient for previous layer (pre-gradient descent)
+        # dLda_prev = dLda * dadz * W^T
+        dLda_prev = delta @ self.W.T
 
-        # weight gradient (chain rule)
-        dzdW = x.T
-        dLdW = dzdW @ dLdz / batch_size # .outer() to fix matrix shape mismatch
+        ## calculate components for gradient descent
+        # per-sample bias gradient (dLdb)
+        # dLdb = delta * dzdb
+        # dLdb = delta * 1
+        dLdb = delta
 
-        # bias gradient (chain rule)
-        dLdb = np.sum(dLdz, axis=0) / batch_size
+        # per-sample weight gradient (dLdW)
+        # dLdW = dLdz * dzdW
+        # dLdW = delta * x
+        dLdW = x.T @ delta # other way round due to shapes
 
-        dLda_prev = dLdz @ self.W.T
+        ## calculate mini-batch gradient
+        # bias component
+        dLdb_batch = np.sum(dLdb, axis=0) / batch_size
 
-        # apply gradient descent
-        self.gradient_descent(dLdW, dLdb)
+        # weight component
+        dLdW_batch = dLdW / batch_size
+
+        ## apply mini-batch gradient descent
+        self.gradient_descent(dLdW_batch, dLdb_batch)
+
+        ## backpropagate upstream gradient to preceding layer
         return dLda_prev
     
     def gradient_descent(self, dLdW: np.ndarray, dLdb: np.ndarray) -> None:
@@ -112,23 +159,54 @@ class Layer:
         self.b -= LEARNING_RATE * dLdb
 
 class OutputLayer(Layer):
-    def __init__(self, biases: np.ndarray, weights: np.ndarray, nodes: int) -> None:
+    def __init__(self, biases: np.ndarray, weights: np.ndarray) -> None:
         super().__init__(biases, weights)
     
     def activation_function(self, z: np.ndarray) -> np.ndarray:
-        return softmax(z)
+        return output_activation_function(z)
 
     def activation_function_derivative(self, z: np.ndarray) -> np.ndarray:
-        return np.ones_like(z)
+        return output_activation_function_derivative(z)
 
-    def linearity(self, x: np.ndarray) -> np.ndarray:
-        return super().linearity(x)
+    def get_preactivation(self, x: np.ndarray) -> np.ndarray:
+        return super().get_preactivation(x)
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         return super().forward(x)
 
-    def backward(self, x: np.ndarray, z: np.ndarray, dLda: np.ndarray, batch_size: int) -> np.ndarray:
-        return super().backward(x, z, dLda, batch_size)
+    def backward(self, a: np.ndarray, x: np.ndarray, y: np.ndarray, batch_size: int) -> np.ndarray:
+        ## compute delta (dLdz) (NOTE: HARDCODED FOR SOFTMAX + CROSS-ENTROPY)
+        # dLdz = dLda * dadz
+        delta = a - y
+
+        ## calculate upstream gradient for previous layer (pre-gradient descent)
+        # dLda_prev = dLda * dadz * W^T
+        # dLda_prev = delta * W^T
+        dLda_prev = delta @ self.W.T
+
+        ## calculate components for gradient descent
+        # per-sample bias gradient (dLdb)
+        # dLdb = dLdz * dzdb
+        # dLdb = delta * 1
+        dLdb = delta
+
+        # per-sample weight gradient (dLdW)
+        # dLdW = dLdz * dzdW
+        # dLdW = delta * x
+        dLdW = x.T @ delta # other way round due to shapes
+
+        ## calculate mini-batch gradient
+        # bias component
+        dLdb_batch = np.sum(dLdb, axis=0) / batch_size
+
+        # weight component
+        dLdW_batch = dLdW / batch_size
+
+        ## apply mini-batch gradient descent
+        self.gradient_descent(dLdW_batch, dLdb_batch)
+
+        ## backpropagate upstream gradient to preceding layer
+        return dLda_prev
 
     def gradient_descent(self, dLdW: np.ndarray, dLdb: np.ndarray) -> None:
         super().gradient_descent(dLdW, dLdb)
