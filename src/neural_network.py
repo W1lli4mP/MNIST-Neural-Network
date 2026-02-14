a6166084d898768b49b5b57f0147c2e5b69cf74d
EE <williamencabopimentel@gmail.com>
2026-02-14 04:35:22 +0000

    Add dropout layers + helpers for API's separation of concerns

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,5 +1,5 @@
 import numpy as np
-from config import LEARNING_RATE
+from config import LEARNING_RATE, DROPOUT_RATE
 
 # activation functions
 def ReLU(z: np.ndarray) -> np.ndarray:
@@ -69,11 +69,16 @@ class NeuralNetwork:
                 self.layers.append(OutputLayer(biases, weights, l2_lambda))
             else:
                 self.layers.append(Layer(biases, weights, l2_lambda))
+                self.layers.append(Dropout(DROPOUT_RATE))
     
-    def forward(self, x: np.ndarray) -> np.ndarray:
+    def forward(self, x: np.ndarray, training: bool = True) -> np.ndarray:
         self.activations = [x]
         for layer in self.layers:
-            x = layer.forward(x)
+            # filter for dropout layers due to mismatched args
+            if isinstance(layer, Dropout):
+                x = layer.forward(x, training)
+            else:
+                x = layer.forward(x)
             self.activations.append(x)
         return x
     
@@ -95,21 +100,46 @@ class NeuralNetwork:
         for i in range(len(self.layers) - 2, -1, -1):
             current_layer = self.layers[i]
 
-            # retrieve activation from the previous layer's activations
-            a_prev = self.activations[i]
-            a = self.activations[i + 1]
-            
-            # backpropagate to find upstream gradient (pre-gradient descent) for preceding layer and apply gradient descent
-            dLda = current_layer.backward(a, a_prev, dLda, batch_size)
+            # filter for backdrop layers - they only require dLda 
+            if isinstance(current_layer, Dropout):
+                dLda = current_layer.backward(dLda)
+            else:
+                # retrieve activation from the previous layer's activations
+                a_prev = self.activations[i]
+                a = self.activations[i + 1]
+                
+                # backpropagate to find upstream gradient (pre-gradient descent) for preceding layer and apply gradient descent
+                dLda = current_layer.backward(a, a_prev, dLda, batch_size)
 
     def calculate_total_loss(self, loss_data: float) -> float:
         l2_penalty = 0.0
         # total loss is defined as the loss data (original loss) with added decay/2 * sum of squared weights (penalty)
         # L_total = L_data + lambda/2 * sum(W^2)
         for layer in self.layers:
+            # skip dropout layers since they have no weights
+            if isinstance(layer, Dropout):
+                continue
             l2_penalty += np.sum(layer.W ** 2)
         l2_penalty *= (self.l2_lambda / 2)
         return loss_data + l2_penalty
+    
+    # useful helpers for combating dropout layer dependencies
+    def get_trainable_layers(self) -> list:
+        return [layer for layer in self.layers if hasattr(layer, "W")]
+    
+    def get_weights(self) -> list:
+        return [layer.W for layer in self.get_trainable_layers()]
+    
+    def get_biases(self) -> list:
+        return [layer.b for layer in self.get_trainable_layers()]
+
+    def set_weights(self, weights: np.ndarray) -> None:
+        for layer, W in zip(self.get_trainable_layers(), weights):
+            layer.W = W
+    
+    def set_biases(self, biases: np.ndarray) -> None:
+        for layer, b in zip(self.get_trainable_layers(), biases):
+            layer.b = b
 
 class Layer:
     def __init__(self, biases: np.ndarray, weights: np.ndarray, l2_lambda: float = 0.0) -> None:
@@ -223,3 +253,25 @@ class OutputLayer(Layer):
 
     def gradient_descent(self, dLdW: np.ndarray, dLdb: np.ndarray) -> None:
         super().gradient_descent(dLdW, dLdb)
+
+class Dropout:
+    def __init__(self, dropout_rate: float = 0.5) -> None:
+        self.dropout_rate = dropout_rate
+        self.mask = None
+
+    def forward(self, a: np.ndarray, training: bool = True) -> np.ndarray:
+        # don't attempt to apply dropout out of training or when dropout rate is 0 (no effect)
+        if not training or self.dropout_rate == 0:
+            return a
+        
+        # inverted dropout formula
+        # use bernoulli distribution (binomial distribution but when n = 1)
+        self.mask = np.random.binomial(1, 1 - self.dropout_rate, a.shape)
+        return a * self.mask / (1 - self.dropout_rate)
+
+    def backward(self, dLda: np.ndarray) -> np.ndarray:
+        # if there is not mask then dropout was not applied (e.g. when dropout is 0)
+        if self.mask is None:
+            return dLda
+
+        return dLda * self.mask / (1 - self.dropout_rate)
